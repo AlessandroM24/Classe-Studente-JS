e943e06746a226a41a29866e93401d0104fe0631
Alessandro <salessandro.macaluso@itis.pr.it>
2024-02-23 12:57:51 +0100

    Aggiunta/modifica di style.css, index.html e app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,7 +19,80 @@ class Studente {
     }
 }
 
+// Regex per controlli.
+const regexNome = /^[a-zA-Z']{2,}$/;
+const regexCognome = /^[a-zA-Z']{2,}$/;
+const regexMatricola = /^[a-zA-Z0-9]{2,}$/;
 
+// Metodo per controlli
+function verificaNome() {
+    let nome = document.modulo.nome.value;
+
+    if (regexNome.test(nome)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function verificaCognome() {
+    let cognome = document.modulo.cognome.value;
+
+    if (regexCognome.test(cognome)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function verificaEta() {
+    let eta = document.modulo.eta.value;
+
+    if (eta >= 1 && eta <= 130) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function verificaMatricola() {
+    let matricola = document.modulo.matricola.value;
+
+    if (regexMatricola.test(matricola)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function invia(event) {
+    let risultato = document.getElementById("risultato");
+    event.preventDefault();
+
+    if (verificaNome() && verificaCognome() && verificaEta() && verificaMatricola()) {
+        risultato.innerHTML = "Persona aggiunta correttamente";
+        document.modulo.reset();
+    } else {
+        risultato.innerHTML = "Dati non validi";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const studente1 = new Studente("Mario", "Rossi", 20, "MR");
 const studente2 = new Studente("Francesco", "Verdi", 17, "FV");
 
@@ -28,3 +101,4 @@ console.log(studente2.toString()); // Stampa la stringa contenente tutti gli att
 
 console.log("studente1 è maggiorenne? " + studente1.isMaggiorenne()); // Stampa se lo studente è maggiorenne o meno.
 console.log("studente2 è maggiorenne? "+ studente2.isMaggiorenne()); // Stampa se lo studente è maggiorenne o meno.
+*/
